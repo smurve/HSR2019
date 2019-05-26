608568a2b3d608a2493fd909b9adf6914725f5a6
Giersche <Wolfgang.Giersche@zuehlke.com>
2019-05-26 17:24:50 +0200

    last changes

--- nbconvert.sh
+++ nbconvert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm Enterprise_ML_Part_I.slides.html && jupyter nbconvert --to slides Enterprise_ML_Part_I.ipynb \
+rm -f Enterprise_ML_Part_II.slides.html && jupyter nbconvert --to slides Enterprise_ML_Part_II.ipynb \
 --post serve \
 --SlidesExporter.reveal_scroll=True \
 --SlidesExporter.reveal_transition=none  \
